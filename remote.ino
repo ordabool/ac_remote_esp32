b886e8f5ca353ebf1c9e351a4e9353654dc0d763
Or Dabool <or.dabool@gmail.com>
2022-06-06 14:04:07 +0300

    Managed to send a constant signal to Ors AC (Turn on and off)

--- remote.ino
+++ remote.ino
@@ -5,17 +5,27 @@
 #include <IRtext.h>
 #include <IRutils.h>
 #include <string.h>
+#include <IRsend.h>
 
 int receiver = 13;
+int transmitter = 33;
+int switchButton = 27;
 const uint16_t MinUnknownSize = 12;
 
 IRrecv irrecv(receiver);
 decode_results results;
 
+IRsend irsend(transmitter);
+
+int lastButtonState = LOW;
+int currentButtonState;
+
 void setup() {
-Serial.begin(9600);
-irrecv.setUnknownThreshold(MinUnknownSize);
-irrecv.enableIRIn();
+    Serial.begin(9600);
+    irrecv.setUnknownThreshold(MinUnknownSize);
+    irrecv.enableIRIn();
+    irsend.begin();
+    pinMode(switchButton, INPUT_PULLUP);
 }
 
 void loop() {
@@ -25,5 +35,14 @@ void loop() {
         Serial.println(resultToHexidecimal(&results));
         irrecv.resume();
     }
+
+    currentButtonState = digitalRead(switchButton);
+    if (lastButtonState == LOW && currentButtonState == HIGH) {
+        irsend.send(AIRWELL, 0x2704001AE, kAirwellBits, kAirwellMinRepeats);
+        Serial.println("Clicked!");
+        delay(3000);
+    }
+
+    lastButtonState = currentButtonState;
     delay(500);
 }
