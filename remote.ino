1110dc8c4d9ffad5f2e797a76fa9b1d89ac91443
Or Dabool <or.dabool@gmail.com>
2022-07-01 19:29:48 +0300

    Used Electra library

--- remote.ino
+++ remote.ino
@@ -6,6 +6,8 @@
 #include <IRutils.h>
 #include <string.h>
 #include <IRsend.h>
+#include "Electra-AC-Remote/electraAcRemoteEncoder.h"
+#include "Electra-AC-Remote/electraAcRemoteEncoder.c"
 
 int receiver = 13;
 int transmitter = 33;
@@ -20,12 +22,16 @@ IRsend irsend(transmitter);
 int lastButtonState = LOW;
 int currentButtonState;
 
+ac my_ac;
+int fanState = 1;
+
 void setup() {
     Serial.begin(9600);
     irrecv.setUnknownThreshold(MinUnknownSize);
     irrecv.enableIRIn();
     irsend.begin();
     pinMode(switchButton, INPUT_PULLUP);
+
 }
 
 void loop() {
@@ -37,12 +43,22 @@ void loop() {
     }
 
     currentButtonState = digitalRead(switchButton);
-    if (lastButtonState == LOW && currentButtonState == HIGH) {
-        irsend.send(AIRWELL, 0x2704001AE, kAirwellBits, kAirwellMinRepeats);
-        Serial.println("Clicked!");
-        delay(3000);
+    // if (lastButtonState == LOW && currentButtonState == HIGH) {
+    if (true) {
+        // irsend.send(AIRWELL, 0x2704001AE, kAirwellBits, kAirwellMinRepeats);
+        // Serial.println("Clicked!");
+
+        int* code = getCodes(&my_ac, fanState, COOL, 23, ON, SWING_OFF);
+        fanState = 4 - fanState;
+
+        Serial.println("Code: ");-
+        Serial.println(my_ac.fullState);
+        Serial.println(*code);
+
+        irsend.send(AIRWELL, my_ac.fullState, kAirwellBits, kAirwellMinRepeats); 
+        
     }
 
     lastButtonState = currentButtonState;
-    delay(500);
+        delay(2000);
 }
